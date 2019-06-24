885957ac8494854b2035c8a5ff8a2f44ed568ace
Mahdi Moradi <mahdi.moradi.w@gmail.com>
2019-06-24 20:38:51 +0430

    closing problem fixed

--- src/com/jpotify/controller/PanelManager.java
+++ src/com/jpotify/controller/PanelManager.java
@@ -417,7 +417,7 @@ public class PanelManager extends ListenerManager implements PlayerListener {
             SimpleAttributeSet center = new SimpleAttributeSet();
             StyleConstants.setAlignment(center, StyleConstants.ALIGN_CENTER);
             doc.setParagraphAttributes(0, doc.getLength(), center, false);
-            jFrame.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+//            jFrame.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
             jFrame.add(jTextPane);
             jFrame.setVisible(true);
 
